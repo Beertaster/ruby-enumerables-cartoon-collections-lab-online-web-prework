2051eedeb60c48446069d3b98e8d81921db81f46
William Stevens <wstevens.hornet@gmail.com>
2019-09-08 21:12:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,13 +10,7 @@ end
 
 def long_planteer_calls(lpc)# code an argument here
   # Your code here
-  answer = false
-  lpc.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-    answer
+  lpc.any? {}
 end
 
 def find_the_cheese# code an argument here
